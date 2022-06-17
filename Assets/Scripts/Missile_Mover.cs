ef0f67d2c625e7a3b973cd206f07067ff042222f
steve rector <steve.rector@datadoghq.com>
2022-06-17 11:37:04 -0700

    Adds Sprite_Missile Prefab + functionality
    Adds Sprite_Missile to Prefabs folder.
    Adds Missile_Mover to Scripts folder that is attached to the prefab and is responsible for move missile forward when instantiated

---
+++ Assets/Scripts/Missile_Mover.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile_Mover : MonoBehaviour
+{
+
+    // missileSpeed to alter how fast missile moves
+    [SerializeField] float missileSpeed = 1f;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        MoveForward();
+    }
+
+
+    /// <summary>
+    /// moves missile forward, will be called in start method so the effect is
+    /// achieved right when the missile is instantiated by the shooter object
+    /// </summary>
+    void MoveForward()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = transform.right * missileSpeed;
+    }
+
+}

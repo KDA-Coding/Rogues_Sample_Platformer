3c1777ee8cc2a378c7f78f18c3e83026d1c1b332
steve rector <steve.rector@datadoghq.com>
2022-06-19 20:36:47 -0700

    Adds Exit Gates that go to next level, or first level if no other levels available
    Adds Sprite_Exit_Portal prefab with LevelExit.cs script attached.
    
    on trigger of the exit, a coroutine starts where a particlesystem is played for a length of time changeable in the inspector, and then the new level is loaded at the end of the coroutine
    
    Also adds rudimentary build settings for scene0 sandbox and steve sandbox so i could make sure the transitions worked

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+
+    [SerializeField] float timeToDelay = 1f;
+    ParticleSystem pSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pSystem = GetComponent<ParticleSystem>();   
+    }
+
+
+    /// <summary>
+    /// loads new level when the objects collider is triggered
+    /// NOTE: need to edit layers or create tag logic to avoid bullets triggering
+    /// this 
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+        LoadNextLevel();
+    }
+
+
+    /// <summary>
+    /// if there is a next level to load, loads that level, otherwise loads the
+    /// first level in the build 
+    /// </summary>
+    IEnumerator LoadNextLevel()
+    {
+
+        pSystem.Play();
+
+        yield return new WaitForSecondsRealtime(timeToDelay);
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    }
+
+   
+}
